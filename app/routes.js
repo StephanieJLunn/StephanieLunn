046336569ea3708c84590c9173a4c7edf533e55e
StephanieJLunn <lunn.stephaniej@gmail.com>
2017-01-20 12:47:07 -0500

    Changed some of the views and routes so that the app.get sends to cleaner landing pages

--- app/routes.js
+++ app/routes.js
@@ -6,8 +6,19 @@ module.exports = function(app) {
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
-	app.get('*', function(req, res) {
+	app.get('/', function(req, res) {
+		res.render("default");
+	});
+	
+	//Obtain the specific landing pages
+	app.get('/', function(req,res) { res.sendfile('./public/index.html')});
+	app.get('/about', function(req,res) { res.sendfile('./public/views/about.html')});
+	app.get('/hci', function(req,res) { res.sendfile('./public/views/hci.html')});
+
+	//Prior app.get
+	/*
+		app.get('*', function(req, res) {
 		res.sendfile('./public/index.html');
 	});
-
+	*/
 };
